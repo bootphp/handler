477d4856c4e1a6ed2b209a90e73ead2f5db14cd8
Lalit Tanwar <lalit.tanwar07@gmail.com>
2017-06-25 03:52:42 +0530

    Added Function Name space

--- AppLoader.php
+++ AppLoader.php
@@ -6,10 +6,48 @@
  * Time: 2:28 AM
  */
 
-namespace bootphp\loader;
+namespace bootphp\loader {
+
+    abstract class AppLoader
+    {
+        private static $__SCANNED__PARAMS__ = false;
+
+        public abstract function invoke();
+
+        public function __get($name)
+        {
+            if (isset($_GET[$name])) {
+                return $_GET[$name];
+            }
+            return null;
+        }
+
+        private function _populate_params_()
+        {
+            if (!self::$__SCANNED__PARAMS__) {
+                foreach ($this as $key => $value) {
+                    if (isset($_REQUEST[$key])) {
+                        $this->{$key} = $_REQUEST[$key];
+                    }
+                }
+                self::$__SCANNED__PARAMS__ = true;
+                return true;
+            }
+            return false;
+        }
+
+        public function execute()
+        {
+            $this->_populate_params_();
+            $this->invoke();
+        }
 
 
-class AppLoader
-{
+        public function __invoke()
+        {
+            return $this->execute();
+        }
+    }
 
 }
+
---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: LT
+ * Date: 25/06/17
+ * Time: 3:40 AM
+ */
+
+namespace bootphp;
+
+function printline()
+{
+    echo "print line";
+}
+
+class functions
+{
+    static function printline()
+    {
+        echo "print line";
+    }
+}
